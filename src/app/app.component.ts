7435692bafd743a25c3b52e8564b3cb1cecef9fa
Wangbl12138 <82312366+Wangbl12138@users.noreply.github.com>
2023-02-13 20:08:57 +0800

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,8 +15,8 @@ export class AppComponent {
         `https://api.openai.com/v1/completions`,
         {
           model: 'text-davinci-003',
-          prompt: '你好',
-          max_tokens: 100,
+          prompt: '英雄联盟中，那五位辅助英雄独自击杀坦克最快？连招是什么',
+          max_tokens: 1000,
           temperature: 0,
           top_p: 1,
           n: 1,
@@ -26,7 +26,7 @@ export class AppComponent {
         {
           headers: {
             Authorization:
-              'Bearer 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',
+              'Bearer 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',
           },
         }
       )
